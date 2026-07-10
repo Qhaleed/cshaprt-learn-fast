38f79d3d23cfad8df1ecc628ba06b30f156a1f20
res <reslykadiri@gmail.com>
2026-07-10 23:00:49 +0800

    oop rrt

---
+++ Player.cs
@@ -0,0 +1,12 @@
+
+// blueprint 
+public class Player
+{
+    public string Name;
+    public int Health = 100;
+
+    public void Attack()
+    {
+        Console.WriteLine("{0} attacks!", Name );
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,16 +1,53 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Console.WriteLine("Hello, World!");
+//
+//
+// string name = "Resham";
+//
+// Console.WriteLine(name);
+// Console.WriteLine('b');
+//
+// Console.WriteLine(123);
+// Console.WriteLine(0.25F);
+//
+// string firstName = "Res";
+// char userOption;
+// int gameScore;
+// decimal particlesPerMillion;
+// bool processedCustomer;
+//
+//
+//
+
+// string name = "bob";
+// int number = 3;
+// double myNumber = 34.4;
+
+//
+//
+// Console.WriteLine("Hello, {0}! You have {1} messages " +
+//               "in your inbox. The temperature is" +
+//               " {2} celsius", name, number, myNumber);
+//
+//
+// string[] fruits = {
+//     "apple",
+//     "orange","bananas"};
+// foreach (string fruit in fruits)
+// {
+//     Console.WriteLine(fruit);
+// }
+
+//
+// Stack<int> stack = new Stack<int>();
+//
+// stack.Push(1);
+// stack.Push(2);
+// stack.Push(5);
+// Console.WriteLine(stack.Peek());
+// Console.WriteLine(stack.Count());
 
 
-string name = "Resham";
 
-Console.WriteLine(name);
-Console.WriteLine('b');
-
-Console.WriteLine(123);
-Console.WriteLine(0.25F);
-
-string firstName = "Res";
-char userOption;
-int gameScore;
-decimal particlesPerMillion;
-bool processedCustomer;
+Player player = new Player();
+player.Name = "Res";
+player.Attack();
