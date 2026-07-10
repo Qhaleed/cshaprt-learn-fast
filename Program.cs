1d2b99a9da028def8be5dfa12d5aebec415d2df2
res <reslykadiri@gmail.com>
2026-07-10 22:34:38 +0800

    dasdsa

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Hello, World!");
+
+
+string name = "Resham";
+
+Console.WriteLine(name);
+Console.WriteLine('b');
+
+Console.WriteLine(123);
+Console.WriteLine(0.25F);
+
+string firstName = "Res";
+char userOption;
+int gameScore;
+decimal particlesPerMillion;
+bool processedCustomer;
