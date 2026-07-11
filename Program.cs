f4ae85a773e6089d06fb7018a025aa9bc762a5be
res <reslykadiri@gmail.com>
2026-07-11 18:24:49 +0800

    solved some more leetcodes

--- Program.cs
+++ Program.cs
@@ -48,6 +48,153 @@
 
 
 
-Player player = new Player();
-player.Name = "Res";
-player.Attack();
+// Player player = new Player();
+// player.Name = "Res";
+// player.Attack();
+
+
+
+// Learning dictionaries in c#
+
+
+
+
+
+// Dictionary<int, string> students = new Dictionary<int, string>();
+
+// example what if we are finding an item?
+//
+// string target = "{";
+//
+// students.Add(1,"res");
+// students.Add(2,"jon");
+// students.Add(3,"mus");
+// students.Add(4, "{");
+// if (students.ContainsValue(target))
+// {
+//     Console.WriteLine("value found");
+// }
+// else
+// {
+//     Console.WriteLine("value not found");
+// }
+//
+
+// another one
+
+// string[] student = ["res", "sam", "dawn"];
+//
+// Dictionary<int, string> student_dictionary = new Dictionary<int, string>();
+// // I want to add this into a dictionary
+//
+// for (int i = 0; i < student.Length; i++)
+// {
+//     student_dictionary.Add(i, student[i]);
+// }
+//
+//
+// if (student_dictionary.TryGetValue(1, out string name))
+// {
+//     Console.WriteLine(name);
+// }
+//
+// foreach (var item in student_dictionary)
+// {
+//     Console.WriteLine(item);
+// }
+
+// string target = "res";
+// if (student_dictionary.ContainsValue(target))
+// {
+//     Console.WriteLine("Target found");
+//     Console.WriteLine("Changing its id to 2x");
+// }
+
+// Dictionary <int,int> dict = new Dictionary<int,int>();
+// int[] nums = [1,2,3,1];
+//
+// for (int i = 0; i < nums.Length; i++)
+// {
+//     Console.WriteLine(nums[i]);
+//     if (dict.ContainsValue(nums[i]))
+//     {
+//         Console.WriteLine("repeated");
+//         return;
+//     }
+//     dict.Add(nums[i], nums[i]);
+//     
+// }
+//
+// ;
+// Console.WriteLine("no repeated values");
+
+
+
+// HashSet<int> hash = new HashSet<int>();
+//
+// hash.Add(1);
+// hash.Add(2);
+// hash.Add(3);
+//
+// Console.WriteLine(hash.Count);
+
+// reassigning vals breh
+
+using System.ComponentModel;
+
+int[] nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+List<int> new_nums = new();
+for (int i = 0; i < nums.Length; i++)
+{
+    if (!new_nums.Contains(nums[i]))
+    {
+       new_nums.Add(nums[i]);
+    }
+}
+
+foreach (var i in new_nums)
+{
+Console.WriteLine(i);
+    
+}
+
+Console.WriteLine(new_nums.Count);
+
+
+// int k = 0;
+//
+// HashSet<int> set = new HashSet<int>(nums);
+// foreach (int i in nums)
+// {
+//     set.Add(i);
+// }
+//
+// k = set.Count;
+// for (int i = 0; i < nums.Length; i++)
+// {
+//     for (int j =  1 + i; j < nums.Length -1; j++)
+//     {
+//         int temp;
+//         if (nums[i] == nums[j])
+//         {
+//             temp = nums[j];
+//             nums[j] = nums[j + 1];
+//             nums[j + 1] = temp;
+//         }
+//         
+//     }
+// }
+//
+// foreach (var s in nums)
+// {
+//         Console.WriteLine(s);
+// }
+// Console.WriteLine("------------");
+// Console.Write(k);
+// for (int i = 0; i < duplicate_counter - 1; i++)
+// {
+//     Console.WriteLine(nums[i]);
+// }
+
+
+
